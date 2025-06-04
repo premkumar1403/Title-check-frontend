dd0b7020e234858e1e98cc25d184a023ab4482d9
premkumar <kumarprem75715@gmail.com>
2025-06-04 16:14:21 +0530

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
-    allowedHosts:["title-check-frontend.onrender.com","192.168.0.131:5173"]
-    
+    allowedHosts:["title-check-frontend.onrender.com"]
   },
 });
